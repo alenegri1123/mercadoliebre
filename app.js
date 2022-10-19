41aba0e9b5aa5d531c8e06abf2fda6da9273255c
alenegri1123 <alemnegri84@gmail.com>
2022-10-19 20:33:58 -0300

    comiteando

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.post('/', function (req, res) {
+    return res.sendFile(req.body)
+})
+
+const port = process.env.PORT || 3004;
+app.listen(port,() => console.log('Servidor corriendo en el puerto${port} '));
+
+
+app.listen(3223, () => {
+    console.log("servidor Meliebre funcionando papaa")
+})
